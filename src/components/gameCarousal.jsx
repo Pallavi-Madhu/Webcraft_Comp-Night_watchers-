e7d6c8b8182830584dc14f651dbeabfb95bfae72
Ajay-Krishna00 <enthusiastajay00@gmail.com>
2025-01-25 10:52:46 +0530

    second commit

---
+++ src/components/gameCarousal.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+
+export default function GameCarousal() {
+  return (
+    <Box bg="blue.500" color="white" p={4} >
+      <h1>Game Carousal</h1>
+    </Box>
+  )
+
+}
